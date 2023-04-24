48b0790724c4ec37de578bb902d207555672fa03
Diego Zamora <diego.dgl10@ciencias.unam.mx>
2023-04-24 14:55:19 -0600

    pruebas terminadas

--- Practica2/src/Programa2.java
+++ Practica2/src/Programa2.java
@@ -6,6 +6,14 @@ public class Programa2 {
     public static void main(String[] args) {
         Pruebas pruebas = new Pruebas();
 
-        pruebas.prueba1();
+        String barra = "";
+        barra += "------------------------------------------------";
+        barra = "\n\n" +barra+ "<>" +barra+ "\n";
+
+        System.out.println("PRUEBA 1:\n" + pruebas.prueba1() + barra);
+        System.out.println("PRUEBA 2:\n" + pruebas.prueba2() + barra);
+        System.out.println("PRUEBA 3:\n" + pruebas.prueba3() + barra);
+        System.out.println("PRUEBA 4:\n" + pruebas.prueba4() + barra);
+        System.out.println("PRUEBA 5:\n" + pruebas.prueba5());
     }
 }
--- Practica2/src/Pruebas.java
+++ Practica2/src/Pruebas.java
@@ -21,6 +21,8 @@ public class Pruebas {
 
     private SetCover setCover = new SetCover();
 
+    private String salida = "";
+
     /**
      * Constructor vacio.
      */
@@ -31,41 +33,233 @@ public class Pruebas {
     /**
      * Prueba numero 1
      */
-    public void prueba1() {
+    public String prueba1() {
         List<String> elemX;
         elemX = new ArrayList<String>(Arrays.asList("1","2","3","4","5","6",
                 "7","8","9","10","11","12"));
         cjtX.setElementos(elemX);
 
 
-        List<String> elem1 = new ArrayList<String>(Arrays.asList("1","2","3","4","5","6"));
+        List<String> elem1;
+        elem1 = new ArrayList<String>(Arrays.asList("1","2","3","4","5","6"));
         cjt1.setElementos(elem1);
-        List<String> elem2 = new ArrayList<String>(Arrays.asList("5","6","8","9"));
+        List<String> elem2;
+        elem2 = new ArrayList<String>(Arrays.asList("5","6","8","9"));
         cjt2.setElementos(elem2);
-        List<String> elem3 = new ArrayList<String>(Arrays.asList("1","4","7","10"));
+        List<String> elem3;
+        elem3 = new ArrayList<String>(Arrays.asList("1","4","7","10"));
         cjt3.setElementos(elem3);
-        List<String> elem4 = new ArrayList<String>(Arrays.asList("2","5","7","8","11"));
+        List<String> elem4;
+        elem4 = new ArrayList<String>(Arrays.asList("2","5","7","8","11"));
         cjt4.setElementos(elem4);
-        List<String> elem5 = new ArrayList<String>(Arrays.asList("3","6","9","12"));
+        List<String> elem5;
+        elem5 = new ArrayList<String>(Arrays.asList("3","6","9","12"));
         cjt5.setElementos(elem5);
-        List<String> elem6 = new ArrayList<String>(Arrays.asList("10","11"));
+        List<String> elem6;
+        elem6 = new ArrayList<String>(Arrays.asList("10","11"));
         cjt6.setElementos(elem6);
-
 
         List<Conjunto<String>> elemF;
         elemF = new ArrayList<Conjunto<String>>(Arrays.asList(cjt1,cjt2,cjt3,cjt4,cjt5,cjt6));
         cjtF.setElementos(elemF);
 
-
-        System.out.println(cjtX.toString());
-        System.out.println("F = {");
-        System.out.println("\t" + cjt1.toString());
-        System.out.println("\t" + cjt2.toString());
-        System.out.println("\t" + cjt3.toString());
-        System.out.println("\t" + cjt4.toString());
-        System.out.println("\t" + cjt5.toString());
-        System.out.println("\t" + cjt6.toString());
-        System.out.println("}");
-        System.out.println(setCover.setCover(cjtX, cjtF));
+        salida += cjtX.toString() + "\n";
+        salida += "F = {" + "\n";
+        salida += "\t" + cjt1.toString() + "\n";
+        salida += "\t" + cjt2.toString() + "\n";
+        salida += "\t" + cjt3.toString() + "\n";
+        salida += "\t" + cjt4.toString() + "\n";
+        salida += "\t" + cjt5.toString() + "\n";
+        salida += "\t" + cjt6.toString() + "\n";
+        salida += "}" + "\n";
+        salida += setCover.setCover(cjtX, cjtF).toString();
+        
+        return salida;
     }
+
+    /**
+     * Prueba numero 2
+     */
+    public String prueba2() {
+        List<String> elemX;
+        elemX = new ArrayList<String>(Arrays.asList("1","2","3","4","5","6",
+                "7","8","9","10","11","12"));
+        cjtX.setElementos(elemX);
+
+        List<String> elem1;
+        elem1 = new ArrayList<String>(Arrays.asList("1","2","3"));
+        cjt1.setElementos(elem1);
+        List<String> elem2;
+        elem2 = new ArrayList<String>(Arrays.asList("5","6","8","9","11","12"));
+        cjt2.setElementos(elem2);
+        List<String> elem3;
+        elem3 = new ArrayList<String>(Arrays.asList("1","4","7","10"));
+        cjt3.setElementos(elem3);
+        List<String> elem4;
+        elem4 = new ArrayList<String>(Arrays.asList("2","5","7","8","9","11"));
+        cjt4.setElementos(elem4);
+        List<String> elem5;
+        elem5 = new ArrayList<String>(Arrays.asList("3","6","9","12"));
+        cjt5.setElementos(elem5);
+
+        List<Conjunto<String>> elemF;
+        elemF = new ArrayList<Conjunto<String>>(Arrays.asList(cjt1,cjt2,cjt3,cjt4,cjt5));
+        cjtF.setElementos(elemF);
+
+        salida += cjtX.toString() + "\n";
+        salida += "F = {" + "\n";
+        salida += "\t" + cjt1.toString() + "\n";
+        salida += "\t" + cjt2.toString() + "\n";
+        salida += "\t" + cjt3.toString() + "\n";
+        salida += "\t" + cjt4.toString() + "\n";
+        salida += "\t" + cjt5.toString() + "\n";
+        salida += "}" + "\n";
+        salida += setCover.setCover(cjtX, cjtF).toString();
+
+        return salida;
+    }
+
+    /**
+     * Prueba numero 3
+     */
+    public String prueba3() {
+        List<String> elemX;
+        elemX = new ArrayList<String>(Arrays.asList("1","2","3","4","5","6",
+                "7","8","9","10","11","12"));
+        cjtX.setElementos(elemX);
+
+        List<String> elem1;
+        elem1 = new ArrayList<String>(Arrays.asList("1","2","3"));
+        cjt1.setElementos(elem1);
+        List<String> elem2;
+        elem2 = new ArrayList<String>(Arrays.asList("1","4","7","10"));
+        cjt2.setElementos(elem2);
+        List<String> elem3;
+        elem3 = new ArrayList<String>(Arrays.asList("10","11","12"));
+        cjt3.setElementos(elem3);
+        List<String> elem4;
+        elem4 = new ArrayList<String>(Arrays.asList("3","6","9","12"));
+        cjt4.setElementos(elem4);
+        List<String> elem5;
+        elem5 = new ArrayList<String>(Arrays.asList("4","5","6","7","8","9"));
+        cjt5.setElementos(elem5);
+        List<String> elem6;
+        elem6 = new ArrayList<String>(Arrays.asList("2","5","8","11"));
+        cjt5.setElementos(elem6);
+
+        List<Conjunto<String>> elemF;
+        elemF = new ArrayList<Conjunto<String>>(Arrays.asList(cjt1,cjt2,cjt3,cjt4,cjt5,cjt6));
+        cjtF.setElementos(elemF);
+
+        salida += cjtX.toString() + "\n";
+        salida += "F = {" + "\n";
+        salida += "\t" + cjt1.toString() + "\n";
+        salida += "\t" + cjt2.toString() + "\n";
+        salida += "\t" + cjt3.toString() + "\n";
+        salida += "\t" + cjt4.toString() + "\n";
+        salida += "\t" + cjt5.toString() + "\n";
+        salida += "\t" + cjt6.toString() + "\n";
+        salida += "}" + "\n";
+        salida += setCover.setCover(cjtX, cjtF).toString();
+
+        return salida;
+    }
+
+
+    /**
+     * Prueba numero 4
+     */
+    public String prueba4() {
+        List<String> elemX;
+        elemX = new ArrayList<String>(Arrays.asList("1","2","3","4","5","6","7","8",
+        "9","10","11","12","13","14","15","16"));
+        cjtX.setElementos(elemX);
+
+        List<String> elem1;
+        elem1 = new ArrayList<String>(Arrays.asList("1","2","3","4","5","6","7","8"));
+        cjt1.setElementos(elem1);
+        List<String> elem2;
+        elem2 = new ArrayList<String>(Arrays.asList("7","8","11","12"));
+        cjt2.setElementos(elem2);
+        List<String> elem3;
+        elem3 = new ArrayList<String>(Arrays.asList("4","8","12","16"));
+        cjt3.setElementos(elem3);
+        List<String> elem4;
+        elem4 = new ArrayList<String>(Arrays.asList("6","7","10","11","14","15"));
+        cjt4.setElementos(elem4);
+        List<String> elem5;
+        elem5 = new ArrayList<String>(Arrays.asList("13","14"));
+        cjt5.setElementos(elem5);
+        List<String> elem6;
+        elem6 = new ArrayList<String>(Arrays.asList("1","5","9","13"));
+        cjt6.setElementos(elem6);
+
+        List<Conjunto<String>> elemF;
+        elemF = new ArrayList<Conjunto<String>>(Arrays.asList(cjt1,cjt2,cjt3,cjt4,cjt5,cjt6));
+        cjtF.setElementos(elemF);
+
+        salida += cjtX.toString() + "\n";
+        salida += "F = {" + "\n";
+        salida += "\t" + cjt1.toString() + "\n";
+        salida += "\t" + cjt2.toString() + "\n";
+        salida += "\t" + cjt3.toString() + "\n";
+        salida += "\t" + cjt4.toString() + "\n";
+        salida += "\t" + cjt5.toString() + "\n";
+        salida += "\t" + cjt6.toString() + "\n";
+        salida += "}" + "\n";
+        salida += setCover.setCover(cjtX, cjtF).toString();
+
+        return salida;
+    }
+
+    /**
+     * Prueba numero 5
+     */
+    public String prueba5() {
+        List<String> elemX;
+        elemX = new ArrayList<String>(Arrays.asList("1","2","3","4","5","6","7","8",
+        "9","10","11","12","13","14","15","16"));
+        cjtX.setElementos(elemX);
+
+        List<String> elem1;
+        elem1 = new ArrayList<String>(Arrays.asList("1","2","5","6"));
+        cjt1.setElementos(elem1);
+        List<String> elem2;
+        elem2 = new ArrayList<String>(Arrays.asList("1","2","3","5","6","7","9","10","11"));
+        cjt2.setElementos(elem2);
+        List<String> elem3;
+        elem3 = new ArrayList<String>(Arrays.asList("11","12","15","16"));
+        cjt3.setElementos(elem3);
+        List<String> elem4;
+        elem4 = new ArrayList<String>(Arrays.asList("6","7","8","10","11","12","14","15","16"));
+        cjt4.setElementos(elem4);
+        List<String> elem5;
+        elem5 = new ArrayList<String>(Arrays.asList("9","13","14"));
+        cjt5.setElementos(elem5);
+        List<String> elem6;
+        elem6 = new ArrayList<String>(Arrays.asList("3","4","7","8"));
+        cjt6.setElementos(elem6);
+        List<String> elem7;
+        elem7 = new ArrayList<String>(Arrays.asList("6","7","10","11"));
+        cjt6.setElementos(elem7);
+
+        List<Conjunto<String>> elemF;
+        elemF = new ArrayList<Conjunto<String>>(Arrays.asList(cjt1,cjt2,cjt3,cjt4,cjt5,cjt6,cjt7));
+        cjtF.setElementos(elemF);
+
+        salida += cjtX.toString() + "\n";
+        salida += "F = {" + "\n";
+        salida += "\t" + cjt1.toString() + "\n";
+        salida += "\t" + cjt2.toString() + "\n";
+        salida += "\t" + cjt3.toString() + "\n";
+        salida += "\t" + cjt4.toString() + "\n";
+        salida += "\t" + cjt5.toString() + "\n";
+        salida += "\t" + cjt6.toString() + "\n";
+        salida += "\t" + cjt7.toString() + "\n";
+        salida += "}" + "\n";
+        salida += setCover.setCover(cjtX, cjtF).toString();
+
+        return salida;
+    }
+
 }
